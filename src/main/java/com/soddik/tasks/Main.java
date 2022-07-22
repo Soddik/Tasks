1b24fe29b36f803b242f56ba8ff9143afb6b5c8d
Soddik <sodd86@gmail.com>
2022-07-22 22:37:04 +0300

    style: +constants

--- src/main/java/com/soddik/tasks/Main.java
+++ src/main/java/com/soddik/tasks/Main.java
@@ -11,14 +11,15 @@ import java.util.logging.Logger;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getSimpleName());
     private static final String PATH = "myFile.csv";
+    private static final String SORTED_PATH = "sortedMyFile.csv";
 
     public static void main(String[] args) {
-        //DataHandler.writeData(PATH, DataHandler.generateData()); //generate large(14_500_000 lines) csv file
+        DataHandler.writeData(PATH, DataHandler.generateData()); //generate large(5_000_000 lines) csv file
         File file = new File(PATH);
         logger.info("====Simple====");
         logger.info("Start: " + new Date());
         AbstractCsvReader simple = new SimpleCsvReaderImpl(file);
-        DataHandler.writeData(simple.parseFile());
+        DataHandler.writeData(SORTED_PATH, simple.parseFile());
         logger.info("End: " + new Date());
     }
 }

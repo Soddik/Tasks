d57653795f0fd95239331dd418356cfac6ac1df6
Soddik <sodd86@gmail.com>
2022-07-22 22:35:21 +0300

    refactoring: Integer.valueOf() first element of array

--- src/main/java/com/soddik/tasks/csv/CustomCsvComparator.java
+++ src/main/java/com/soddik/tasks/csv/CustomCsvComparator.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class CustomCsvComparator implements Comparator<String[]> {
     @Override
     public int compare(String[] o1, String[] o2) {
-        return o1[0].compareTo(o2[0]);
+        return Integer.valueOf(o2[0]).compareTo(Integer.valueOf(o1[0]));
     }
 }

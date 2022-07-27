483f5832d8e4621eb1ae5bf251f06b22be7a269e
Soddik <sodd86@gmail.com>
2022-07-27 14:18:04 +0300

    refactoring: package-private

--- src/main/java/com/soddik/tasks/csv/CustomCsvComparator.java
+++ src/main/java/com/soddik/tasks/csv/CustomCsvComparator.java
@@ -2,7 +2,7 @@ package com.soddik.tasks.csv;
 
 import java.util.Comparator;
 
-public class CustomCsvComparator implements Comparator<String[]> {
+class CustomCsvComparator implements Comparator<String[]> {
     @Override
     public int compare(String[] o1, String[] o2) {
         return Integer.valueOf(o2[0]).compareTo(Integer.valueOf(o1[0]));

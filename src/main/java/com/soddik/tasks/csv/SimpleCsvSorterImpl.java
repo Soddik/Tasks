762220dd093a2744b255b297279f9a9dfefb264e
Soddik <sodd86@gmail.com>
2022-07-27 14:17:23 +0300

    refactoring: writing data in parseFile() SRP :(

--- src/main/java/com/soddik/tasks/csv/SimpleCsvSorterImpl.java
+++ src/main/java/com/soddik/tasks/csv/SimpleCsvSorterImpl.java
@@ -7,33 +7,34 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-public class SimpleCsvReaderImpl implements CsvReader {
-    private final Logger logger = Logger.getLogger(SimpleCsvReaderImpl.class.getSimpleName());
+public class SimpleCsvSorterImpl implements CsvSorter {
+    private final Logger logger = Logger.getLogger(SimpleCsvSorterImpl.class.getSimpleName());
     private final String separator = ";";
-    private final File file;
+    private final File in;
+    private final File out;
 
-    public SimpleCsvReaderImpl(File file) {
-        this.file = file;
+    public SimpleCsvSorterImpl(File in, String out) {
+        this.in = in;
+        this.out = new File(out);
     }
 
     @Override
-    public List<String[]> parseFile() {
-        List<String[]> result = new ArrayList<>();
+    public void parseFile() {
         try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(Files.newInputStream(Paths.get(file.getAbsolutePath())), StandardCharsets.UTF_8))) {
-            result = br.lines()
+                new InputStreamReader(Files.newInputStream(Paths.get(in.getAbsolutePath())), StandardCharsets.UTF_8))) {
+            List<String[]> result = br.lines()
                     .skip(1)
                     .map(line -> line.split(separator))
                     .sorted(new CustomCsvComparator())
                     .collect(Collectors.toList());
+
+            DataHandler.writeData(out, result);
         } catch (IOException e) {
             logger.warning(e.getMessage());
         }
-        return result;
     }
 }
